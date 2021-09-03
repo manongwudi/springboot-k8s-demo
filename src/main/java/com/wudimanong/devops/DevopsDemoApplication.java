b9ff31e265d27c20501e8827cd913065b796ae83
jiangqiao <jiangqiao@mafengwo.com>
2021-09-03 13:29:13 +0800

    init

---
+++ src/main/java/com/wudimanong/devops/DevopsDemoApplication.java
@@ -0,0 +1,15 @@
+package com.wudimanong.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jiangqiao
+ */
+@SpringBootApplication
+public class DevopsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsDemoApplication.class, args);
+    }
+}
